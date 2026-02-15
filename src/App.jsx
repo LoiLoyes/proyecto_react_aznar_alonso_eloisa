a02eba7fae3d5186b04a921829c1a36fe53e7302
Eloisa-Aznar <1801581@alu.murciaeduca.es>
2026-02-15 01:52:36 +0100

    - Cambios para mejorar la legibilidad y otros pequeños cambios

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,20 @@ import Cabecera from './components/cabecera'
 import logo from './assets/logo.svg'
 import Formulario from './components/Formulario'
 import Resultados from './components/Resultados'
-const apiKey = import.meta.env.VITE_APIKEY;
 
 function App() {
 
-  const [municipio, setMunicipio] = useState("")
-
+  const [municipio, setMunicipio] = useState("");
+  
   return (
     <>
       <Cabecera logo={logo} titulo="App Meteorológica React"/>
-      <body className="body">
-        <h2>Consulta el tiempo en tu municipio</h2>
-        <Formulario setMunicipio={setMunicipio}/>
-        <Resultados municipio={municipio}/>
-        <footer className="footer">
-          <p>Desarrollado por Eloisa Aznar Alonso 2ºDAW</p>
-        </footer>
-      </body>
-      
+
+      <h2>Consulta el tiempo en tu municipio</h2>
+
+      <Formulario setMunicipio={setMunicipio}/>
+
+      <Resultados municipio={municipio}/>
     </>
   )
 }
